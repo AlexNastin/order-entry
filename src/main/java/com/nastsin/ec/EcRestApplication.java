8f60b47d52599035aefbedc8a92ff049e6cacb07
alex.nastsin <alexnastin1992@gmail.com>
2019-09-30 12:31:42 +0200

    init commit

---
+++ src/main/java/com/nastsin/ec/EcRestApplication.java
@@ -0,0 +1,12 @@
+package com.nastsin.ec;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EcRestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EcRestApplication.class, args);
+    }
+}
